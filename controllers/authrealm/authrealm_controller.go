c8b330d3c4ffc0830efaf65c6081944d0621a0b9
Dominique Vernier <dvernier@redhat.com>
2021-09-04 09:18:47 -0400

    Fix rbac
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- controllers/authrealm/authrealm_controller.go
+++ controllers/authrealm/authrealm_controller.go
@@ -45,8 +45,8 @@ type AuthRealmReconciler struct {
 // +kubebuilder:rbac:groups=auth.identitatem.io,resources={dexservers,dexclients},verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources={namespaces,secrets,serviceaccounts},verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="apps",resources={deployments},verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources={clusterrolebindings},verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources={clusterroles},verbs=get;list;watch;create;update;patch;delete;escalate;bind
+// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources={clusterrolebindings,rolebindings},verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources={clusterroles,roles},verbs=get;list;watch;create;update;patch;delete;escalate;bind
 // +kubebuilder:rbac:groups="apiextensions.k8s.io",resources={customresourcedefinitions},verbs=get;list;create;update;patch;delete
 
 func (r *AuthRealmReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
