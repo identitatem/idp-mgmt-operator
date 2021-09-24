4c6929772444449a13b45d1c826de199fd9bf51b
Chris Ahl <cahl@redhat.com>
2021-09-24 13:17:26 -0400

    pretty up  output
    Signed-off-by: Chris Ahl <cahl@redhat.com>

--- hack/generate-cr.sh
+++ hack/generate-cr.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 # Copyright Red Hat
 
+# Color codes for bash output
+BLUE='\e[36m'
+GREEN='\e[32m'
+RED='\e[31m'
+YELLOW='\e[33m'
+CLEAR='\e[39m'
+if [[ "$COLOR" == "False" || "$COLOR" == "false" ]]; then
+    BLUE='\e[39m'
+    GREEN='\e[39m'
+    RED='\e[39m'
+    YELLOW='\e[39m'
+fi
+
+printf "\n${BLUE}Gathering OpenShift cluster URL...${CLEAR}\n"
+
 export NAME=${NAME:-"authrealm-sample"}
 export NS=${NS:-"authrealm-sample-ns"}
 export APPS=$(oc get infrastructure cluster -ojsonpath='{.status.apiServerURL}' | cut -d':' -f2 | sed 's/\/\/api/apps/g')
@@ -20,6 +35,7 @@ fi
 
 GITHUB_APP_CLIENT_SECRET_B64=`echo -n "$GITHUB_APP_CLIENT_SECRET" | $BASE64`
 
+printf "\n${BLUE}Generating YAML ...${CLEAR}\n\n"
 
 cat > ${THE_FILENAME} <<EOF
 ---
@@ -87,15 +103,16 @@ spec:
         - ${GITHUB_APP_CLIENT_ORG}
 EOF
 
-echo "File ${THE_FILENAME} is generated and ready to \"oc apply -f ${THE_FILENAME}\""
-echo ""
-echo "Please ensure you have an entry in GitHub under Settings > Developer Settings > OAuth Apps "
-echo "for Client ID ${GITHUB_APP_CLIENT_ID} contains:"
-echo "    - Homepage URL: https://console-openshift-console.${APPS}"
-echo "    - Authorization callback URL: https://${ROUTE_SUBDOMAIN}.${APPS}/callback"
-echo "prior to running the \"oc\" command."
-echo ""
-echo "Add the following labels to any managed cluster you want in the cluster set ${NAME}-clusterset:"
-echo "    authdeployment=east"
-echo "    cluster.open-cluster-management.io/clusterset=${NAME}-clusterset"
-echo ""
+printf "\n${BLUE}Before using the generated YAML:${CLEAR}\n\n"
+
+printf "${BLUE}1) Ensure there is an entry in GitHub under Settings > Developer Settings > OAuth Apps${CLEAR}\n"
+printf "${BLUE}for Client ID ${GREEN}${GITHUB_APP_CLIENT_ID}${BLUE} contains:${CLEAR}\n"
+printf "    - ${GREEN}Homepage URL:${YELLOW} https://console-openshift-console.${APPS}${CLEAR}\n"
+printf "    - ${GREEN}Authorization callback URL:${YELLOW} https://${ROUTE_SUBDOMAIN}.${APPS}/callback${CLEAR}\n"
+printf "${BLUE}prior to running the ${GREEN}oc apply ${BLUE} command shown below.\n\n${CLEAR}"
+
+printf "${BLUE}2) Add the following labels to any managed cluster you want in the cluster set $GREEN}${NAME}-clusterset${BLUE}:${CLEAR}\n"
+printf "    ${GREEN}authdeployment=east${CLEAR}\n"
+printf "    ${GREEN}cluster.open-cluster-management.io/clusterset=${NAME}-clusterset${CLEAR}\n\n"
+
+printf "\n${BLUE}File${GREEN} ${THE_FILENAME}${BLUE} is generated.  Apply using \"${GREEN}oc apply -f ${THE_FILENAME}${BLUE}\"${CLEAR}\n\n"
