71dd5441d9e52492c7d031bec9ed0c3d6e92d547
Chris Ahl <cahl@redhat.com>
2021-10-06 13:36:38 -0400

    update placement to use v1beta1
    Signed-off-by: Chris Ahl <cahl@redhat.com>

--- hack/generate-cr.sh
+++ hack/generate-cr.sh
@@ -46,13 +46,13 @@ metadata:
     control-plane: controller-manager
   name: ${NS}
 ---
-apiVersion: cluster.open-cluster-management.io/v1alpha1
+apiVersion: cluster.open-cluster-management.io/v1beta1
 kind: ManagedClusterSet
 metadata:
   name: ${NAME}-clusterset
   namespace: ${NS}
 ---
-apiVersion: cluster.open-cluster-management.io/v1alpha1
+apiVersion: cluster.open-cluster-management.io/v1beta1
 kind: Placement
 metadata:
   name: ${NAME}-placement
@@ -64,7 +64,7 @@ spec:
         matchLabels:
           authdeployment: east
 ---
-apiVersion: cluster.open-cluster-management.io/v1alpha1
+apiVersion: cluster.open-cluster-management.io/v1beta1
 kind: ManagedClusterSetBinding
 metadata:
   name: ${NAME}-clusterset
@@ -109,7 +109,7 @@ printf "${BLUE}1) Ensure there is an entry in GitHub under Settings > Developer 
 printf "${BLUE}for Client ID ${GREEN}${GITHUB_APP_CLIENT_ID}${BLUE} contains:${CLEAR}\n"
 printf "    - ${GREEN}Homepage URL:${YELLOW} https://console-openshift-console.${APPS}${CLEAR}\n"
 printf "    - ${GREEN}Authorization callback URL:${YELLOW} https://${ROUTE_SUBDOMAIN}.${APPS}/callback${CLEAR}\n"
-printf "${BLUE}prior to running the ${GREEN}oc apply ${BLUE} command shown below.\n\n${CLEAR}"
+printf "${BLUE}prior to running the ${GREEN}oc apply${BLUE} command shown below.\n\n${CLEAR}"
 
 printf "${BLUE}2) Add the following labels to any managed cluster you want in the cluster set ${GREEN}${NAME}-clusterset${BLUE}:${CLEAR}\n"
 printf "    ${GREEN}authdeployment=east${CLEAR}\n"
