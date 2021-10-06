9223c96e92a2348201733a636dce01d4a2f58c27
Dominique Vernier <dvernier@redhat.com>
2021-10-06 15:01:08 -0400

    Change idp-name by authrealm-name
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- controllers/placementdecision/dex.go
+++ controllers/placementdecision/dex.go
@@ -149,7 +149,7 @@ func (r *PlacementDecisionReconciler) createDexClient(authRealm *identitatemv1al
 
 	host = strings.Replace(host, "api", "apps", 1)
 
-	redirectURI := fmt.Sprintf("%s://oauth-openshift.%s/oauth2callback/%s", u.Scheme, host, idp.Name)
+	redirectURI := fmt.Sprintf("%s://oauth-openshift.%s/oauth2callback/%s", u.Scheme, host, authRealm.Name)
 	dexClient.Spec.RedirectURIs = []string{redirectURI}
 	switch dexClientExists {
 	case true:
