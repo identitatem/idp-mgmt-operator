171ac261f16a7102bea7b7d101e0f59ffb977d9e
Dominique Vernier <dvernier@redhat.com>
2021-10-29 13:51:46 -0400

    Remove code smell
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- controllers/authrealm/dex.go
+++ controllers/authrealm/dex.go
@@ -32,9 +32,10 @@ const (
 
 func (r *AuthRealmReconciler) syncDexCRs(authRealm *identitatemv1alpha1.AuthRealm) error {
 	r.Log.Info("syncDexCRs", "AuthRealm.Name", authRealm.Name, "AuthRealm.Namespace", authRealm.Namespace)
-	//TODO this test should maybe be in a webhook to avoid the creation of an invalid CR
 	if len(authRealm.Spec.IdentityProviders) < 1 {
-		return giterrors.WithStack(fmt.Errorf("the identityproviders array of the authrealm %s can have one and only one element", authRealm.Name))
+		return giterrors.WithStack(
+			fmt.Errorf("the identityproviders array of the authrealm %s can have one and only one element",
+				authRealm.Name))
 	}
 
 	// Create namespace and Install the dex-operator
