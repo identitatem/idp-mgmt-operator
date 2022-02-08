2fc04686d4e40b8c906302dd92febebe96dfe9af
Vidya Nambiar <vnambiar@redhat.com>
2022-02-08 13:57:00 -0500

    remove unnecessary logs
    Signed-off-by: Vidya Nambiar <vnambiar@redhat.com>

--- test/e2e/cypress-ui/cypress/tests/sampleOAuthTest.spec.js
+++ test/e2e/cypress-ui/cypress/tests/sampleOAuthTest.spec.js
@@ -14,8 +14,6 @@ describe('Sample Test Suite to test creation and deletion of a GitHub OAuth app'
 
     it('Empty Test', { tags: ['empty'] }, function () {
         cy.log('OAuth app name: ', Cypress.env('OPTIONS_GH_OAUTH_APP_NAME'));
-        cy.log('OAuth app client ID', Cypress.env('OPTIONS_GH_OAUTH_CLIENT_ID'));
-        cy.log('OAuth app client Secret', Cypress.env('OPTIONS_GH_OAUTH_CLIENT_SECRET'));
         cy.log('Nothing Done Here');
     });
 })
